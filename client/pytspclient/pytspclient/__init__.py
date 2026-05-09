75a49c51579eb92a1604a95508c0f319c416ee9b
AlexaZhou <AlexaZhou@163.com>
2026-05-09 20:03:12 +0800

    chore: bump version to 0.2.6

--- client/pytspclient/pytspclient/__init__.py
+++ client/pytspclient/pytspclient/__init__.py
@@ -9,7 +9,7 @@ from .types import (
 )
 from .adapters import LLMAdapter, TspAnthropicAdapter, TspOpenAIAdapter
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __all__ = [
     "TSPClient",
     "TSPRequest", "TSPResponse", "TSPEvent", "TSPException",
